ddf2800c0ca47e55ed743436f9d0ba90c5b57835
Fidan03 <xelilovafidan61@gmail.com>
2025-12-07 02:42:32 +0000

    .

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,40 @@
 #include "main.h"
 
 /**
-	* main - prints _putchar followed by a new line
-	*
+	* main - prints _putchar
 	* Return: 0
 	*/
 int main(void)
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-	_putchar(str[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
-	return 0;
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
